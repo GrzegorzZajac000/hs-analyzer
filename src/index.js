558cd652ae366df04e9cefeaf1c47c1e2e37a05b
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2022-01-23 17:27:53 +0100

    index.js init

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.scss';
+import App from './components/App';
+
+let appElement = null;
+
+if (typeof document !== 'undefined') {
+  appElement = document.getElementById('app');
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  appElement
+);
