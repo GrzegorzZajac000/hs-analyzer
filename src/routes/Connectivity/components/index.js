a4a05a42cc142ff1f6ec77b20f1acf997b7bbb5e
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2022-02-05 10:54:20 +0100

    Connectivity WIP; Can't be done without Node server @todo future

--- src/routes/Connectivity/components/index.js
+++ src/routes/Connectivity/components/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import '../styles/Connectivity.scss';
 import PropTypes from 'prop-types';
 
+// WARNING
+// Can't be done without Node server
+// @todo future
+
 class Connectivity extends React.Component {
   constructor (props) {
     super(props);
+
+    this.consoleRef = React.createRef();
+
     this.generateListenAddresses = this.generateListenAddresses.bind(this);
+    this.checkPortState = this.checkPortState.bind(this);
+    this.ping = this.ping.bind(this);
+  }
+
+  checkPortState (host, port) {
+    console.log('checkPortState', host, port);
+  }
+
+  ping (host, port) {
+    console.log('ping', host, port);
   }
 
   generateListenAddresses () {
@@ -19,7 +36,7 @@ class Connectivity extends React.Component {
       return (
         <div className='connectivity-address' key={i}>
           <div className='container-fluid'>
-            <div className='row'>
+            <div className='row connectivity-address-api'>
               <div className='col-3'>
                 <h2>Hotspot IP</h2>
                 <h3>{ip}</h3>
@@ -30,9 +47,9 @@ class Connectivity extends React.Component {
               </div>
             </div>
             <div className='row'>
-              <div className='col-6'>
-                <button className='btn btn-decor btn-lg'>Check port state</button>
-                <button className='btn btn-decor btn-lg'>Ping</button>
+              <div className='col-6 connectivity-address-buttons'>
+                <button className='btn btn-decor btn-lg' onClick={() => this.checkPortState(ip, port)}>Check port state</button>
+                <button className='btn btn-decor btn-lg' onClick={() => this.ping(ip, port)}>Ping</button>
               </div>
             </div>
           </div>
@@ -46,8 +63,8 @@ class Connectivity extends React.Component {
       <section className='connectivity route-section'>
         {this.generateListenAddresses()}
         <pre>
-          <code>
-            ...
+          <code ref={this.consoleRef}>
+            Ready for connectivity test...
           </code>
         </pre>
       </section>
