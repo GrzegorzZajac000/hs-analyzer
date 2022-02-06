b60a5f65a63edbb874a9d57df6f5306598e05be6
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2022-02-06 13:56:14 +0100

    DateGenerator

---
+++ src/api/DateGenerator.js
@@ -0,0 +1,16 @@
+const DateGenerator = date => {
+  const d = new Date(date);
+  const year = d.getUTCFullYear();
+
+  let month = d.getUTCMonth() + 1;
+  month = month.toString();
+  month = month.length <= 1 ? `0${month}` : month;
+
+  let day = d.getUTCDate();
+  day = day.toString();
+  day = day.length <= 1 ? `0${day}` : day;
+
+  return `${year}-${month}-${day}`;
+};
+
+export default DateGenerator;
