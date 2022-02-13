a8826f016f940e9ecb9eb6eba4fa56ed38570ed3
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2022-02-13 12:44:38 +0100

    siteReducer HS methods

--- src/modules/siteReducer.js
+++ src/modules/siteReducer.js
@@ -3,29 +3,22 @@ const SET_MOBILE_MENU_CLOSED = 'SET_MOBILE_MENU_CLOSED';
 const SET_GENERAL_INFO = 'SET_GENERAL_INFO';
 const SET_HS_INFO = 'SET_HS_INFO';
 const SET_DAYS_INFO = 'SET_DAYS_INFO';
+const ADD_HS_TO_LIST = 'ADD_HS_TO_LIST';
+const REMOVE_HS_FROM_LIST = 'REMOVE_HS_FROM_LIST';
+const USE_HS = 'USE_HS';
+const SHOW_HS_MODAL = 'SHOW_HS_MODAL';
+const HIDE_HS_MODAL = 'HIDE_HS_MODAL';
 
-export const setMobileMenuOpened = () => ({
-  type: SET_MOBILE_MENU_OPENED
-});
-
-export const setMobileMenuClosed = () => ({
-  type: SET_MOBILE_MENU_CLOSED
-});
-
-export const setGeneralInfo = generalInfo => ({
-  type: SET_GENERAL_INFO,
-  generalInfo
-});
-
-export const setHsInfo = hsInfo => ({
-  type: SET_HS_INFO,
-  hsInfo
-});
-
-export const setDaysInfo = daysInfo => ({
-  type: SET_DAYS_INFO,
-  daysInfo
-})
+export const setMobileMenuOpened = () => ({ type: SET_MOBILE_MENU_OPENED });
+export const setMobileMenuClosed = () => ({ type: SET_MOBILE_MENU_CLOSED });
+export const setGeneralInfo = generalInfo => ({ type: SET_GENERAL_INFO, generalInfo });
+export const setHsInfo = hsInfo => ({ type: SET_HS_INFO, hsInfo });
+export const setDaysInfo = daysInfo => ({ type: SET_DAYS_INFO, daysInfo });
+export const addHSToList = hsObject => ({ type: ADD_HS_TO_LIST, hsObject });
+export const removeHSFromList = hsIndex => ({ type: REMOVE_HS_FROM_LIST, hsIndex });
+export const useHS = hsIndex => ({ type: USE_HS, hsIndex });
+export const showHSModal = () => ({ type: SHOW_HS_MODAL });
+export const hideHSModal = () => ({ type: HIDE_HS_MODAL });
 
 const initialState = {
   mobileMenuOpened: false,
@@ -36,7 +29,10 @@ const initialState = {
     geolocation: { countries: 0, cities: 0 }
   },
   hsInfo: {},
-  daysInfo: {}
+  daysInfo: {},
+  hsList: [],
+  currentHS: 0,
+  hsModal: false
 };
 
 export default function counter (state = initialState, action) {
@@ -55,6 +51,29 @@ export default function counter (state = initialState, action) {
 
   case SET_DAYS_INFO:
     return { ...state, daysInfo: action.daysInfo };
+
+  case ADD_HS_TO_LIST: {
+    const hsList = state.hsList;
+    hsList.push(action.hsObject);
+
+    return { ...state, hsList };
+  }
+
+  case REMOVE_HS_FROM_LIST: {
+    const hsList = state.hsList;
+    hsList.splice(action.hsIndex, 1);
+
+    return { ...state, hsList };
+  }
+
+  case USE_HS:
+    return { ...state, currentHS: action.hsIndex };
+
+  case SHOW_HS_MODAL:
+    return { ...state, hsModal: true };
+
+  case HIDE_HS_MODAL:
+    return { ...state, hsModal: false };
 
   default:
     return state;
