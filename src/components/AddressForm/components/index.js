2ea29cfadc48f75f26e8e9a10b26c65f7e95e6ac
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2022-03-16 19:53:19 +0100

    AddressForm - added flag to HS

--- src/components/AddressForm/components/index.js
+++ src/components/AddressForm/components/index.js
@@ -5,6 +5,7 @@ import HeliumAPI from '../../../api/HeliumAPI';
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import HSName from '../../../utilities/HSName';
+import Flag from 'react-world-flags';
 
 class AddressForm extends React.Component {
   constructor (props) {
@@ -39,7 +40,11 @@ class AddressForm extends React.Component {
       .then(res => {
         const hs = {};
         hs.value = res.data.data.address;
-        hs.label = `${HSName.toView(res.data.data.name)} - ${res.data.data.geocode.long_city}, ${res.data.data.geocode.long_country}`;
+        hs.label = (
+          <React.Fragment>
+            <Flag className='flag' code={res.data.data.geocode.short_country} height={16} /> {HSName.toView(res.data.data.name)}
+          </React.Fragment>
+        );
         hs.data = res.data.data;
 
         return this.props.addHSToList(hs);
