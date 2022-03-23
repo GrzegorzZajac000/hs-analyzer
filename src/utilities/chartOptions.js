252edb69387a2a0b382551ba176df62695c96996
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2022-03-23 19:15:36 +0100

    Chart watermark

--- src/utilities/chartOptions.js
+++ src/utilities/chartOptions.js
@@ -1,3 +1,24 @@
+import { Chart } from 'chart.js';
+
+const chartWatermark = {
+  id: 'chart-watermark',
+  afterDraw: chart => {
+    const ctx = chart.ctx;
+    const width = chart.chartArea.width;
+
+    ctx.globalAlpha = 0.75;
+
+    ctx.font = 'normal normal 400 14px Lato, sans-serif';
+    ctx.fillStyle = '#fff';
+    ctx.textAlign = 'right';
+    ctx.fillText('hs-analyzer.com', width + 15, 30);
+
+    ctx.globalAlpha = 1;
+  }
+};
+
+Chart.register(chartWatermark);
+
 const chartOptions = onClick => {
   return {
     plugins: {
@@ -22,7 +43,8 @@ const chartOptions = onClick => {
       },
       legend: {
         display: false
-      }
+      },
+      chartWatermark
     },
     scale: {
       ticks: {
