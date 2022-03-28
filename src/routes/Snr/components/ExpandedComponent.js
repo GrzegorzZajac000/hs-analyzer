b7c8408fbcf3bc808c65521210ed06600749ca00
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2022-03-28 14:26:22 +0200

    SNR ExpandedComponent as separate file

---
+++ src/routes/Snr/components/ExpandedComponent.js
@@ -0,0 +1,42 @@
+import { GetTimeAgo } from '../../../utilities';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ExpandedComponent = ({ data }) => {
+  const rows = data.witnesses.map((w, i) => {
+    return (
+      <tr key={i}>
+        <td>{w.frequency}</td>
+        <td>{w.channel}</td>
+        <td>{w.signal}</td>
+        <td>{w.snr}</td>
+        <td>{w.signal - w.snr}</td>
+        <td>{GetTimeAgo(w.timestamp / 1000000)}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Frequency</th>
+          <th>Channel</th>
+          <th>Signal</th>
+          <th>SNR</th>
+          <th>Noise</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  );
+}
+
+ExpandedComponent.propTypes = {
+  data: PropTypes.object
+};
+
+export default ExpandedComponent;
--- src/routes/Snr/components/index.js
+++ src/routes/Snr/components/index.js
@@ -1,48 +1,12 @@
 import React from 'react';
 import '../styles/Snr.scss';
-import { BaseComponent, GetTimeAgo } from '../../../utilities';
+import { BaseComponent } from '../../../utilities';
 import PropTypes from 'prop-types';
 import HeliumAPI from '../../../api/HeliumAPI';
 import { toast } from 'react-toastify';
 import { getDistance } from 'geolib';
 import DataTable from 'react-data-table-component';
-
-const ExpandedComponent = ({ data }) => {
-  const rows = data.witnesses.map((w, i) => {
-    return (
-      <tr key={i}>
-        <td>{w.frequency}</td>
-        <td>{w.channel}</td>
-        <td>{w.signal}</td>
-        <td>{w.snr}</td>
-        <td>{w.signal - w.snr}</td>
-        <td>{GetTimeAgo(w.timestamp / 1000000)}</td>
-      </tr>
-    );
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Frequency</th>
-          <th>Channel</th>
-          <th>Signal</th>
-          <th>SNR</th>
-          <th>Noise</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows}
-      </tbody>
-    </table>
-  );
-}
-
-ExpandedComponent.propTypes = {
-  data: PropTypes.object
-};
+import ExpandedComponent from './ExpandedComponent';
 
 class Snr extends BaseComponent {
   constructor (props) {
