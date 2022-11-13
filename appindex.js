3d30f8d0fd97c7caa5dea780ff6476e3c9a6a50b
Grzegorz Zając <grzegorz.zajac000@gmail.com>
2022-11-13 13:57:04 +0100

    cors

--- appindex.js
+++ appindex.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.options('*', (req, res) => {
+  res.send(200);
+});
+
 app.use(express.static('dist'));
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
